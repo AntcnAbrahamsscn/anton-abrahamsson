77ae3ed246564a5da27cc163f3aeadcc698d1975
Anton Abrahamsson <abrahamsson37@gmail.com>
2024-03-26 15:28:14 +0100

    Fixed minor responsive bugs

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -8,13 +8,14 @@ const items = [
         img: hangbotImage,
         header: "Hangbot the game",
         text: "Hangbot is a project developed by me and two other students for a school assignment. Our objective was to create a hangman game using HTML, CSS, and JS. The game features an list of over 18,000 random Swedish words. To add a competitive edge, we implemented a high score list that stores data using local storage.",
-        url: "https://wanderingkitty.github.io/BionicCriminals/",
+        url: "https://wanderingkitty.github.io/BionicCriminals/", ghUrl: "https://github.com/wanderingkitty/BionicCriminals"
     },
     {
         img: pokemonImage,
         header: "Pokémon Team Manager",
         text: "Pokémon Team Manager is an individual school project. My task was to use the pokeapi.com API and create a Pokémon management game. The objective was to enable users to search for Pokémon, add them to their team, and have the option to move them to a reserve list.",
         url: "https://antcnabrahamsscn.github.io/pok--team-manager/#",
+        ghUrl: "https://github.com/AntcnAbrahamsscn/pok--team-manager"
     },
 ];
 
@@ -62,6 +63,7 @@ export default function Case() {
                             >
                                 {textVisibility[index] ? "close" : "about"}
                             </button>
+                            <a href={item.ghUrl} className="read-more-btn play-button">{<i class="fa-brands fa-github"></i>}</a>
                             <a href={item.url} className="read-more-btn play-button">{<i class="fa-solid fa-play"></i>}</a>
                             </div>
                         </div>
