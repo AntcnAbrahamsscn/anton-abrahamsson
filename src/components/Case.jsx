3e0727cfa5f09ad5c712e5e686ddb9dbe710bff2
Anton Abrahamsson <abrahamsson37@gmail.com>
2024-08-23 10:13:14 +0200

    Hollow text

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -1,36 +1,9 @@
 import { useState, useEffect, useRef } from "react";
-import hangbotImage from "../assets/hangbot-cover.png";
-import pokemonImage from "../assets/pokemon-cover.png";
-import terraImage from "../assets/terra-protein-package.jpg";
 import { useFadeIn } from "../scripts/useFadeIn";
+import {items} from './../data/item-list'
 import "../stylesheets/case.css";
 
-const items = [
-    {
-        img: hangbotImage,
-        header: "Hangbot the game",
-        text: "Hangbot is a project developed by me and two other students for a school assignment. Our objective was to create a hangman game using HTML, CSS, and JS. The game features an list of over 18,000 random Swedish words. To add a competitive edge, we implemented a high score list that stores data using local storage.",
-        url: "https://wanderingkitty.github.io/BionicCriminals/",
-        ghUrl: "https://github.com/wanderingkitty/BionicCriminals",
-        skills: "JS | HTML | CSS"
-    },
-    {
-        img: pokemonImage,
-        header: "Pokémon Team Manager",
-        text: "Pokémon Team Manager is an individual school project. My task was to use the pokeapi.com API and create a Pokémon management game. The objective was to enable users to search for Pokémon, add them to their team, and have the option to move them to a reserve list.",
-        url: "https://antcnabrahamsscn.github.io/pok--team-manager/#",
-        ghUrl: "https://github.com/AntcnAbrahamsscn/pok--team-manager",
-        skills: "JS | HTML | CSS"
-    },
-    {
-        img: terraImage,
-        header: "Terra Insect Protein",
-        text: "Terra - Insect Protein, an imaginary brand created for a final thesis in Media and Communication studies, explores the visual representation of insect-derived protein powder supplements. Consumer interviews informed Terra's development, focusing on purchasing priorities and considerations for protein supplements.",
-        url: "https://drive.google.com/file/d/1oWziQd1lJsjQ41JrIN7tCtuRMqBv98kP/view?usp=drive_link",
-        // ghUrl: "https://github.com/AntcnAbrahamsscn/pok--team-manager",
-        skills: "FIGMA | INDESIGN | ILLUSTRATOR"
-    },
-];
+
 
 export default function Case() {
     const [activeIndex, setActiveIndex] = useState(0);
---
+++ src/data/item-list.js
@@ -0,0 +1,40 @@
+import hangbotImage from "../assets/hangbot-cover.png";
+import pokemonImage from "../assets/pokemon-cover.png";
+import terraImage from "../assets/terra-protein-package.jpg";
+import hollowImage from "../assets/hollow-bg-img.png";
+
+const items = [
+    {
+        img: hollowImage,
+        header: "Hollow Webshop",
+        text: "For this project, I created a fully functional and responsive webshop for Hollow, my fictional surf brand. The website is designed with user experience in mind, ensuring easy navigation for customers who want to browse and purchase surf-related products. I also added an admin feature that lets you create, delete and edit the products.",
+        url: "https://antcnabrahamsscn.github.io/hollow-webshop/",
+        ghUrl: "https://github.com/AntcnAbrahamsscn/hollow-webshop",
+        skills: "REACT | FIRESTORE",
+    },
+    {
+        img: hangbotImage,
+        header: "Hangbot the game",
+        text: "Hangbot is a project developed by me and two other students for a school assignment. Our objective was to create a hangman game using HTML, CSS, and JS. The game features an list of over 18,000 random Swedish words. To add a competitive edge, we implemented a high score list that stores data using local storage.",
+        url: "https://wanderingkitty.github.io/BionicCriminals/",
+        ghUrl: "https://github.com/wanderingkitty/BionicCriminals",
+        skills: "JS | HTML | CSS",
+    },
+    {
+        img: pokemonImage,
+        header: "Pokémon Team Manager",
+        text: "Pokémon Team Manager is an individual school project. My task was to use the pokeapi.com API and create a Pokémon management game. The objective was to enable users to search for Pokémon, add them to their team, and have the option to move them to a reserve list.",
+        url: "https://antcnabrahamsscn.github.io/pok--team-manager/#",
+        ghUrl: "https://github.com/AntcnAbrahamsscn/pok--team-manager",
+        skills: "JS | HTML | CSS",
+    },
+    {
+        img: terraImage,
+        header: "Terra Insect Protein",
+        text: "Terra - Insect Protein, an imaginary brand created for a final thesis in Media and Communication studies, explores the visual representation of insect-derived protein powder supplements. Consumer interviews informed Terra's development, focusing on purchasing priorities and considerations for protein supplements.",
+        url: "https://drive.google.com/file/d/1oWziQd1lJsjQ41JrIN7tCtuRMqBv98kP/view?usp=drive_link",
+        skills: "FIGMA | INDESIGN | ILLUSTRATOR",
+    },
+];
+
+export { items };
